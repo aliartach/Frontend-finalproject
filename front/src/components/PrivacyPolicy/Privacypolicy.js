209013a517408873341d0f5fe99b462cac647955
Ali Artach <aliartach96@gmail.com>
2024-03-05 11:14:36 +0200

    privacypolicy fixed

--- front/src/components/PrivacyPolicy/Privacypolicy.js
+++ front/src/components/PrivacyPolicy/Privacypolicy.js
@@ -3,10 +3,16 @@ import "./Privacypolicy.css"
 
 const Privacypolicy = () => {
   return (
-    <div>
-      <div className='divprivacypolicy'>
-        <h4 className='titleprivacypolicy'>Privacy Policies</h4>
-      </div>
+    <div class="privacy-policy">
+        <h2>Privacy Policy</h2>
+        <p class="policy-section">At Choco Heaven, we are committed to protecting the privacy and security of our customers.</p>
+        <p class="policy-section">This Privacy Policy explains how we collect, use, and safeguard your personal information when you visit our website (<span class="website-url">www.choco-heaven.com</span>), make a purchase, or interact with us.</p>
+        <h3 class="policy-section">Information We Collect</h3>
+        <p class="policy-section">We collect personal information that you provide to us voluntarily when you place an order, sign up for our newsletter, or participate in promotions or surveys. This information may include your name, email address, shipping address, and payment details.</p>
+        <h3 class="policy-section">How We Use Your Information</h3>
+        <p class="policy-section">We use the information we collect to process your orders, communicate with you about your purchases, send you marketing communications (if you opt-in), and improve our services.</p>
+        <h3 class="policy-section">Data Security</h3>
+        <p class="policy-section">We take the security of your personal information seriously and implement appropriate technical and organizational measures to protect it.</p>
     </div>
   )
 }
